a1940ca75ef045a96f9d715088006b0e65856a02
Janis Vahsteins <janisvahsteins@gmail.com>
2017-05-19 10:39:32 +0300

    removed old code and added bootstrap

--- crit_calc.js
+++ crit_calc.js
@@ -1,18 +1,14 @@
 
 function main(){
-//Total agility on character
+//Total agility on character. It cannot be negative value
 var agility = parseInt(document.getElementById("agility_inp").value,10);
-//parseInt(prompt("What is your total agility?"),10);
-//Agility cannot be negative
 if(agility < 0){
     agility === 0;
 }
 // Weapon crit rate
 var baseCrit = parseInt(document.getElementById("base_crit").value,10);
-//parseInt(prompt("Weapon or spell critchance: 1/"),10);
 //Total +crits on character from equipment
 var equipCrit = parseInt(document.getElementById("plus_crit").value,10);
-//parseInt(prompt("Your current +crit bonus?"),10);
 //Calculates critical chance
 var critChance = finalCrit();
 //Critchance cannot be worse than baseCrit-eqipCrit and lower than 1/2
@@ -24,6 +20,8 @@ if (critChance < 2){
 }
 //Return result
 document.getElementById("result").value = critChance;
+
+
 function finalCrit(){
 critChance = Math.floor((baseCrit-equipCrit)*((Math.E*1.1) / Math.log(agility+12)));
 return critChance;
