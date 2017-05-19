9599786bfdca02b260546f3f85ae34485bfa1972
Janis Vahsteins <janisvahsteins@gmail.com>
2017-05-19 18:00:07 +0300

    BS forms, updated html and js

--- crit_calc.js
+++ crit_calc.js
@@ -1,29 +1,28 @@
 
 function main(){
 //Total agility on character. It cannot be negative value
-var agility = parseInt(document.getElementById("agility_inp").value,10);
-if(agility < 0){
-    agility === 0;
-}
-// Weapon crit rate
-var baseCrit = parseInt(document.getElementById("base_crit").value,10);
+    var agility = parseInt(document.getElementById("agility_inp").value,10);
+    if(agility < 0 || agility === NaN){
+    agility = 0;
+    }
+// Weapon or spell crit rate
+    var baseCrit = parseInt(document.getElementById("base_crit").value,10);
 //Total +crits on character from equipment
-var equipCrit = parseInt(document.getElementById("plus_crit").value,10);
+    var equipCrit = parseInt(document.getElementById("plus_crit").value,10);
 //Calculates critical chance
-var critChance = finalCrit();
-//Critchance cannot be worse than baseCrit-eqipCrit and lower than 1/2
-if (critChance > baseCrit-equipCrit){
-    critChance = baseCrit-critChance;
-}
-if (critChance < 2){
-    critChance = 2;
-}
+    var critChance = finalCrit();
+//Critchance cannot be worse than baseCrit-eqipCrit, lower than 1/2 and higher than 1/basecrit
+    if (critChance > baseCrit-equipCrit){
+        critChance = baseCrit-critChance;
+    }
+    if (critChance < 2){
+        critChance = 2;
+    }
 //Return result
-document.getElementById("result").value = critChance;
+    document.getElementById("result").value = critChance;
 
-
-function finalCrit(){
-critChance = Math.floor((baseCrit-equipCrit)*((Math.E*1.1) / Math.log(agility+12)));
-return critChance;
-}
+    function finalCrit(){
+    crit = Math.floor((baseCrit-equipCrit)*((Math.E*1.1) / Math.log(agility+12)));
+    return crit;
+    }
 }
